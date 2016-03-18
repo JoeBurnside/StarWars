481d692273dd28edf579d013d0f69e5a8b7a8496
JoeBurnside <joethetav@gmail.com>
2016-03-18 14:53:52 +0000

    Added

---
+++ shiv.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+document.createElement('header');
+document.createElement('nav');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('button');
+document.createElement('content');
+document.createElement('image');
+document.createElement('solidblock')
